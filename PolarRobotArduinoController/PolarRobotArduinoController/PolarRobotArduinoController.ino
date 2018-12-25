b65e61986dc85e226c945f2aac088d5fd2096607
adawsonelli <dawsonelli@wisc.edu>
2018-12-25 00:05:05 -0600

    updated to accomodate call and response format

--- PolarRobotArduinoController/PolarRobotArduinoController/PolarRobotArduinoController.ino
+++ PolarRobotArduinoController/PolarRobotArduinoController/PolarRobotArduinoController.ino
@@ -4,45 +4,91 @@
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
-// to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(400, 2);
+//setup adafruit stepper motors
+int PPR = 400; //pulses per revolution
+Adafruit_StepperMotor *thetaMotor = AFMS.getStepper(PPR, 1); //(M1 and M2)
+Adafruit_StepperMotor *rMotor = AFMS.getStepper(PPR, 2);     //(M3 and M4)
 
-// AccelStepper Code
-// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+//setup wrapper functions for forward and backwards use
 void forwardstep1() {
-  myMotor->onestep(FORWARD, DOUBLE);
+  thetaMotor->onestep(FORWARD, DOUBLE);  // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
 }
-
 void backwardstep1() {
-  myMotor->onestep(BACKWARD, DOUBLE);
+  thetaMotor->onestep(BACKWARD, DOUBLE);
+}
+void forwardstep2() {
+  rMotor->onestep(FORWARD, DOUBLE);  // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+}
+void backwardstep2() {
+  rMotor->onestep(BACKWARD, DOUBLE);
 }
 
-AccelStepper Astepper1(forwardstep1, backwardstep1); // Alternate constructor
+//AccelStepper objects with Alternative Constructor
+AccelStepper ArStepper(forwardstep1, backwardstep1); // Alternate constructor
+AccelStepper AthetaStepper(forwardstep2, backwardstep2); // Alternate constructor
+
+
+//state variables
+bool atDestination = false;
+bool waitForPacket = true;
+char rTarget = 0;
+char thetaTarget = 0;
 
 void setup()
-{
-   Serial.begin(9600);           // set up Serial library at 9600 bps
-   Serial.println("Stepper test!");
+{  
+    //setup serial
+     Serial.begin(115200);
+     
+    //init adafruit servo drivers
+    AFMS.begin();  // create with the default frequency 1.6KHz
 
-   AFMS.begin();  // create with the default frequency 1.6KHz
-   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-   Astepper1.setMaxSpeed(1000.0);
-   Astepper1.setAcceleration(100.0);
-   Astepper1.setSpeed(1000);
-   //Astepper1.moveTo(324);
-   //Astepper1.setSpeed(80);
+    //setup servos
+    ArStepper.setMaxSpeed(300.0);
+    ArStepper.setSpeed(1000);
+    ArStepper.setAcceleration(100.0);
+   
+    AthetaStepper.setMaxSpeed(300.0);
+    AthetaStepper.setSpeed(1000);
+    AthetaStepper.setAcceleration(100.0);
+  
 }
-
-// bruhhhhhhhhhhhhh
 
 void loop()
 {
+   
+   //wait for new command to come in
+   while (waitForPacket)
+   {
+      //wait for fixed number of packets to accumulate
+      if(Serial.available() > 1) //2 chars
+      {
+        waitForPacket = false;
+        //read in packet
+        rTarget = Serial.read();
+        thetaTarget = Serial.read();
+        
+        // set stepper destinations and velocities
+        ArStepper.moveTo(rTarget);
+        AthetaStepper.moveTo(thetaTarget);
+      }
+   }
+   
+    while( ~atDestination)
+    {
+      //continue to run
+      ArStepper.run();
+      AthetaStepper.run();
 
-Astepper1.runSpeed();
-//delay(1);
+      //check if the servos are at their destinations
+      atDestination = (ArStepper.distanceToGo() == 0) && (AthetaStepper.distanceToGo() == 0);
+    }
 
+    //send request for next destination
+    Serial.write(0xFF); // write all good. 
+
+    //reset flags
+    waitForPacket = true;
+    atDestination = false;
+  
 }
