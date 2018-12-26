26c5b674cb9050b75e6366342c149f54150b9e1c
adawsonelli <dawsonelli@wisc.edu>
2018-12-25 23:30:13 -0600

    wrote code for velocity saturation on the robot if too aggressive of a move is specified.

--- PolarRobotArduinoController/PolarRobotArduinoController.ino
+++ PolarRobotArduinoController/PolarRobotArduinoController.ino
@@ -30,69 +30,132 @@ AccelStepper AthetaStepper(forwardstep2, backwardstep2); // Alternate constructo
 
 
 //state variables
-bool atDestination = false;
-bool waitForPacket = true;
-char rTarget = 0;
-char thetaTarget = 0;
+int freq = 2;                          //2hz = .5sec period
+int stepperVmx = 1000;                 //mx velocity of the stepper
+bool atDestination = false;            //is r state at rTarget?
+bool waitForPacket = true;             //are we waiting for packet?
+ArStepper.setCurrentPosition(0);       //current location of r axis  [steps]
+AthetaStepper.setCurrentPosition(0);   //current location theta axis [steps]
+unsigned int rTarget = 0;              //desired location of r axis in global pulses [steps]
+unsigned int thetaTarget = 0;          //desired location of the theta axis [steps]
+
+void thetaROC()
+//perform rollover correction on theta axis
+{
+
+}
+
+void mainDist()
+//calculate the minimum dist around a circle
+{
+
+}
+
+
+
+
+void setStepperSpeeds()
+/*determine the velocity to command of each stepper. if either velocity value
+ saturates, then make sure the velocity ratio is maintained*/
+{
+  //determine the velocities required for each axis to reach it's destination by next time step
+  int rVel = (rTarget - ArStepper.currentPosition())*freq              //[dsteps /dt]
+  int tVel = (thetaTarget - AthetaStepper.currentPosition())*freq      //[dsteps/dt]
+
+  //handle when either motor saturates, maintain speed ratio
+  if (abs(rVel) > stepperVmx  || abs(tVel) > stepperVmx)
+  {
+      if (abs(rVel) >= abs(tVel))
+      {
+        ArStepper.setSpeed(stepperVmx);
+        AthetaStepper.setSpeed(stepperVmx*tVel/abs(rVel));    //scale slower speed
+      }
+
+      else if(abs(rVel) < abs(tVel))
+      {
+        AthetaStepper.setSpeed(stepperVmx);
+        ArStepper.setSpeed(stepperVmx * rVel / abs(tVel));   //scale slower speed
+      }
+  }
+
+  //otherwise, set motor speeds right away
+  else
+  {
+    ArStepper.setSpeed(rVel);
+    AthetaStepper.setSpeed(rVel);
+  }
+
+}
+
 
 void setup()
-{  
+{
     //setup serial
      Serial.begin(115200);
-     
+
     //init adafruit servo drivers
     AFMS.begin();  // create with the default frequency 1.6KHz
 
     //setup servos
     ArStepper.setMaxSpeed(300.0);
-    ArStepper.setSpeed(100);
+    ArStepper.setSpeed(0);
     ArStepper.setAcceleration(100.0);
-   
+
     AthetaStepper.setMaxSpeed(300.0);
-    AthetaStepper.setSpeed(100);
+    AthetaStepper.setSpeed(0);
     AthetaStepper.setAcceleration(100.0);
-  
+
 }
 
 void loop()
 {
-   
+
    //wait for new command to come in
    while (waitForPacket)
    {
       //wait for fixed number of packets to accumulate
-      if(Serial.available() > 1) //2 chars
+      if(Serial.available() > 4) //2 chars
       {
         //read in packet
-        rTarget = Serial.read();
-        thetaTarget = Serial.read();
+        char rh = Serial.read();
+        char rl = Serial.read();
+        char thetah = Serial.read();
+        char thetal = Serial.read();
+
+        //read counts into an int
+        rTarget = rh << 8 | rl
+        thetaTarget = thetah << 8 | thetal
+
         Serial.println(rTarget,DEC);
         Serial.println(thetaTarget,DEC);
-        
-        // set stepper destinations and velocities
-//        ArStepper.moveTo((int)rTarget);
-//        AthetaStepper.moveTo((int)thetaTarget);
-        
+
+        // set stepper speeds
+        setStepperSpeeds();
+
         //packet successfully transfered
         waitForPacket = false;
       }
    }
-   
-    while( ~atDestination)
-    {
-      //continue to run
-//      ArStepper.run();
-//      AthetaStepper.run();
 
-      //check if the servos are at their destinations
-      atDestination = (ArStepper.distanceToGo() == 0) && (AthetaStepper.distanceToGo() == 0);
+    while(~atDestination)
+    {
+      //check if each axis is at destination:
+      bool rDest = ArStepper.currentPosition() != rTarget
+      bool thetaDest = AthetaStepper.currentPosition() != thetaTarget
+
+      //move one step
+     if (rDest){ArStepper.run();}
+     if (thetaDest){AthetaStepper.run();}
+
+      //check if both servos are at their destinations
+      atDestination = (rDest  && thetaDest);
     }
 
     //send request for next destination
-    Serial.write(0xFF); // write all good. 
+    Serial.write(0xFF); // write all good.
 
     //reset flags
     waitForPacket = true;
     atDestination = false;
-  
+
 }
